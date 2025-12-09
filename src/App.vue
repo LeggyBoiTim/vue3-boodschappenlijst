0b317a1bf147fb9b646d74012df720f2fadc0d8d
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-12-09 14:10:57 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
+import { ref } from 'vue';
 import GroceriesTable from './components/GroceriesTable.vue';
 
-const products = [
+const products = ref([
     { name: 'Rijst', price: 1.00, amount: 0 },
     { name: 'Broccoli', price: 0.99, amount: 0 },
     { name: 'Koekjes', price: 1.20, amount: 0 },
     { name: 'Noten', price: 2.99, amount: 0 }
-];
+]);
 </script>
 
 <template>
